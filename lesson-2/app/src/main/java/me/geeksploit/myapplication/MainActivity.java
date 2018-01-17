6b4f98ecd6acf812ad35018961ad3a00e0c312c5
geeksploit <geeksploit@shootan.com>
2018-01-18 01:27:05 +0300

    Set the Activity's action bar

--- lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
+++ lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
