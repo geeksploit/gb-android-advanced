9a98698ebc0946a439ce1073c9387bce48221f0a
geeksploit <geeksploit@shootan.com>
2018-01-18 16:00:38 +0300

    Add an ActionBarDrawerToggle to the navigation drawer

--- lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
+++ lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package me.geeksploit.myapplication;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -15,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
