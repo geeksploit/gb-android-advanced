1f8fdd1ef9a8a3f2118edb85a595c59e97c7337a
geeksploit <geeksploit@shootan.com>
2018-01-18 16:35:31 +0300

    Implement OnNavigationItemSelectedListener in MainActivity

--- lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
+++ lesson-2/app/src/main/java/me/geeksploit/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package me.geeksploit.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +41,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
